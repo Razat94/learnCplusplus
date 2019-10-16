9efd275bf8785aee682369a24aa1bdaf9acb5294
Raza <Razat51@gmail.com>
2019-10-15 19:37:51 -0700

    added data types chapter

---
+++ content/programs/chapter1/chp1-01.cpp
@@ -0,0 +1,13 @@
+// This file is named chp1-01.cpp
+#include <iostream>
+using namespace std;
+
+int main() {
+   cout << "Hello, World!" << endl;
+   cout << "My name is Raza!" << " I hope you have a great day!" << endl;
+   
+   
+   
+   cout << "Goodbye!";
+   return 0;   
+}
---
+++ content/programs/chapter2/chp2-00.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << 0 << endl;
+    cout << 5 + 5 << endl;
+    cout << 3 - 5 << endl;
+    cout << 3 * 5 << endl;
+    cout << 10 / 5 << endl;
+}
