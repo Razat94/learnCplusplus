b814e90c1e8e67fcdf8c5d7d0289830959df450b
Raza <Razat51@gmail.com>
2019-11-02 22:24:31 -0700

    fixed chapter2 content; added example for booleans

---
+++ content/programs/chapter2/chp2-01.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << true << "is different from" << " " << "true" << endl;
+	cout << "and" << endl;
+	cout << false << "is different from" << " " << "false << endl;
+}
