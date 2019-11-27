9ef0a9170225d11ea1229271103567ffd05f42cd
Raza <Razat51@gmail.com>
2019-11-27 05:09:46 -0800

    saved files

--- content/programs/chapter6/chp6-00.cpp
+++ content/programs/chapter6/chp6-00.cpp
@@ -1,10 +1,7 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-	// Before going over if statements, remember: 
-	// A block of code is literally just some code written inside of { } brackets.
-	
+int main() { // opening brace for main function
 	{
 		// some code
 		cout << "Hello World!" << endl;
@@ -17,10 +14,11 @@ int main() {
 
 	// You can have nested block statements
 	// i.e. a block of code within another block of codes.
+	
 	{
 		cout << "Statement 1 \t";
 		{ 
 			cout << "Statement 2";
 		}
 	}
-}
+} // closing brace for main function.
