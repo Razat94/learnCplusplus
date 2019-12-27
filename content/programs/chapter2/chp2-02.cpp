df1a01f4a7e036d3cebacf7144972e463ddd7bad
Raza <Razat51@gmail.com>
2019-12-26 22:26:00 -0800

    tried to add a pagination nav on Chp. Failed miserably, will try again later

---
+++ content/programs/chapter2/chp2-02.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	/*
+		Ints encompass positive and negative numbers, as well as 0.
+	*/
+    cout << 0 << endl;
+    cout << 9001 << endl;
+    cout >> -9001 << endl;
+    /*
+    	Arithmetic is also possible.
+    */
+    cout << 5 + 5 << endl;
+    cout << 3 - 5 << endl;
+    cout << 3 * 5 << endl;
+    cout << 10 / 5 << endl;
+}
